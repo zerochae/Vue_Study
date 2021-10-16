777eb8fa7df3084f02d830ad35515e9e9d3cb255
zerochae <zerochae@kakao.com>
2021-10-16 21:39:12 +0900

    #Vue Stduy UI Tap

--- study03/src/components/Container.vue
+++ study03/src/components/Container.vue
@@ -1,27 +1,48 @@
 <template>
   <div>
-    <Post :postData="list" v-for="list in postData" :key="list"/>
+    <div v-if="step == 0">
+      <Post :postData="list" v-for="list in postData" :key="list" />
+    </div>
+    <div v-if="step == 1">
+      <!-- 필터선택페이지 -->
+      <div class="upload-image"></div>
+      <div class="filters">
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+      </div>
+    </div>
+
+    <div v-if="step== 2">
+      <!-- 글작성페이지 -->
+      <div class="upload-image"></div>
+      <div class="write">
+        <textarea class="write-box">write!</textarea>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
-import Post from './Post.vue';
+import Post from "./Post.vue";
 
 export default {
-  name : 'Container',
-  data(){
-    return{
-    }
+  name: "Container",
+  data() {
+    return {};
   },
-  components : {
-    Post : Post,
+  components: {
+    Post: Post,
   },
-  props : {
-    postData : Array,
-  }
+  props: {
+    postData: Array,
+    step: Number,
+  },
 };
 </script>
 
 <style>
+@import "./../assets/Container.css";
 </style>
