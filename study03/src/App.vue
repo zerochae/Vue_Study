048062eaaa19d4429bd1d24060b36c16e0e3f751
zerochae <zerochae@kakao.com>
2021-10-17 14:46:09 +0900

    #Vue Study emit

--- study03/src/App.vue
+++ study03/src/App.vue
@@ -5,22 +5,33 @@
         <li>Cancel</li>
       </ul>
       <ul class="header-button-right">
-        <li>Next</li>
+        <li v-if="step == 1" @click="step++">Next</li>
+        <li v-if="step == 2" @click="publish">Publishing</li>
       </ul>
       <img src="./assets/logo.png" class="logo" />
     </div>
 
-    <Container :postData="postData" :step='step' />
+    <Container
+      :postData="postData"
+      :step="step"
+      :uploadImageUrl="uploadImageUrl"
+      @callContent="callContent"
+    />
 
     <button @click="more">더보기</button>
 
     <div class="footer">
       <ul class="footer-button-plus">
-        <input type="file" id="file" class="inputfile" />
+        <input
+          @change="upload"
+          accept="image/*"
+          type="file"
+          id="file"
+          class="inputfile"
+        />
         <label for="file" class="input-plus">+</label>
       </ul>
     </div>
-
   </div>
 </template>
 
@@ -35,8 +46,9 @@ export default {
     return {
       postData: postData,
       count: 0,
-      step : 0,
-
+      step: 0,
+      uploadImageUrl: "",
+      content : "",
     };
   },
   components: {
@@ -64,6 +76,33 @@ export default {
       // .then((result) =>{})
       // .catch((err)=> {})
     },
+    upload(e) {
+      let file = e.target.files;
+      console.log(file);
+      this.uploadImageUrl = URL.createObjectURL(file[0]);
+      console.log(this.uploadImageUrl);
+      this.step = 1;
+      // 이미지 삽입 방법 2가지
+      // 1. FileReader() -> 파일을 글자로 변환해준다
+      // 2. URL.createObjectURL() -> 이미지의 가상 URL을 호스팅해준다
+    },
+    publish() {
+      var posting = {
+        name: "zerochae",
+        userImage: "https://placeimg.com/100/100/arch",
+        postImage: this.uploadImageUrl,
+        likes: "36",
+        date: "Oct 17",
+        liked: false,
+        content: this.content,
+        filter: "perpetua",
+      };
+      postData.unshift(posting);
+      this.step = 0;
+    },
+    callContent(content){
+      this.content = content
+    }
   },
 };
 </script>
