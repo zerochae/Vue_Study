0d3e69f13a5419b135f096196c8609ccca794730
zerochae <zerochae@kakao.com>
2021-11-13 19:22:10 +0900

    login page

---
+++ login/src/components/Console.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="userInput">
+    <span>{{rootText}}</span> <span class="userIn">
+      <input :type="inputType" @keyup.enter="enterInput" v-model="inputText" id="loginInput">
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "console",
+  data() {
+    return {
+      inputText: ""
+    }
+  },
+  mounted(){
+    document.getElementById('loginInput').focus()
+  },
+  props: {
+    rootText : String,
+    inputType : String,
+  },
+  methods: {
+    enterInput() {
+      this.$emit('enterInput', this.inputText)
+      this.inputText = ""
+    }
+  }
+}
+</script>
+
+<style>
+</style>
