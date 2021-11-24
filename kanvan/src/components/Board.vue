a0cc471ad8e55eea8d658e9d124920655cc337d9
zerochae <zerochae@kakao.com>
2021-11-25 01:14:07 +0900

    kanvan

--- kanvan/src/components/Board.vue
+++ kanvan/src/components/Board.vue
@@ -6,17 +6,16 @@
     orientation="horizontal"
     @drop="onColumnDrop($event)"
   >
-    <Draggable
-      class="
-        bg-gray-200
+    <!-- kanvan column :  bg-gray-200
         dark:bg-gray-700
         rounded-lg
         h-full
         w-96
         flex-shrink-0
-        shadow-xl
-      "
-      v-for="(column, index) in scene.children"
+        shadow-xl-->
+    <div
+      class="kanvanContainer"
+      v-for="(column, index) in kanvan.columns"
       :key="index"
     >
       <div class="h-full flex flex-col">
@@ -31,52 +30,59 @@
             text-white
             flex
             space-x-2
+            kanvan-title
           "
         >
-          <span class="text-lg">{{ column.name }}</span>
+          <span class="text-lg">{{ column.name }} </span>
           <button @click="showInput(index)">+</button>
         </div>
         <!-- column -->
-        <Container
-          class="flex-grow overflow-y-auto overflow-x-hidden"
-          orientation="vertical"
-          group-name="col-items"
-          :shouldAcceptDrop="
-            (e, payload) => e.groupName === 'col-items' && !payload.loading
-          "
-          :get-child-payload="getCardPayload(column.id)"
-          :drop-placeholder="{
-            className: `bg-primary bg-opacity-20  
+        <!-- place : `bg-primary bg-opacity-20  
             border-dotted border-2 
-            border-primary rounded-lg mx-4 my-2`,
-            animationDuration: '200',
-            showOnTop: true,
-          }"
-          drag-class="bg-primary dark:bg-primary 
-            border-2 border-primary-hover text-white 
-            transition duration-100 ease-in z-50
-            transform rotate-6 scale-110"
-          drop-class="transition duration-100 
-            ease-in z-50 transform 
-            -rotate-2 scale-90"
-          @drop="(e) => onCardDrop(column.id, e)"
-        >
-          <!-- Items -->
-          <!-- ssh commit test-->
-          <KanbanItem
-            v-for="item in column.children"
-            :key="item.id"
-            :item="item"
-          ></KanbanItem>
-          <input
-            type="text"
-            v-model="input"
-            v-if="scene.children[index].showInput == true"
-            @keydown.enter="addCard(index)"
-          />
-        </Container>
+            border-primary rounded-lg mx-4 my-2`-->
+        <!-- drop : 
+            ease-in z-50 transform-rotate-2 scale-90-->
+        <Draggable>
+          <Container
+            class="flex-grow overflow-y-auto overflow-x-hidden"
+            orientation="vertical"
+            group-name="col-items"
+            :shouldAcceptDrop="
+              (e, payload) => e.groupName === 'col-items' && !payload.loading
+            "
+            :get-child-payload="getCardPayload(column.id)"
+            :dropClass="{
+              className : `card-ghost-drop`,
+              showOnTop : true,
+              animationDuration: `1000`,
+              }"
+            :drop-placeholder="{
+              className: `drop-placeholder`,
+              animationDuration: '200',
+              showOnTop: true,
+            }"
+            :dragClass="`card-ghost-drag`"
+            @drop="(e) => onCardDrop(column.id, e)"
+          >
+            <!-- Items -->
+            <!-- ssh commit test-->
+            <KanbanItem
+              v-for="(item, j) in column.cards"
+              :key="j"
+              :columnIndex="index"
+              :cardIndex="j"
+              :item="item"
+            />
+            <input
+              type="text"
+              v-model="input"
+              v-if="kanvan.columns[index].showInput == true"
+              @keydown.enter="addCard(index)"
+            />
+          </Container>
+        </Draggable>
       </div>
-    </Draggable>
+    </div>
   </Container>
 </template>
 
@@ -84,7 +90,6 @@
 import KanbanItem from "../components/KanbanItem.vue";
 import { Container, Draggable } from "vue3-smooth-dnd";
 import { generateItems, generateWords, applyDrag } from "../utils/helpers";
-
 
 const boardTitle_kor = [
   "화면 미 구현",
@@ -94,24 +99,25 @@ const boardTitle_kor = [
   "API 개발 완료",
 ];
 
-const scene = {
+const kanvan = {
   type: "container",
   props: {
     orientation: "horizontal",
   },
-  children: generateItems(boardTitle_kor.length, (i) => ({
+  columns: generateItems(boardTitle_kor.length, (i) => ({
     id: `column${i}`,
-    type: "container",
+    // type: "container",
     // name: generateWords( boardTitle_kor[i] ),
     name: boardTitle_kor[i],
     showInput: false,
     props: {
-      orientation: "vertical",
+      // orientation: "vertical",
     },
-    children: generateItems(7, (j) => ({
-      type: "draggable",
+    cards: generateItems(7, (j) => ({
+      // type: "draggable",
       id: `${i}-${j}`,
       loading: false,
+      showCardInMenu: false,
       user_name: "zerochae",
       content: generateWords(Math.random() * 12 + 2),
     })),
@@ -128,107 +134,125 @@ export default {
     return {
       openInput: -1,
       input: "test",
-      scene,
+      nowOpen: [-1,-1],
+      kanvan,
       // boardTitle_en,
       boardTitle_kor,
-      listData: [
-        {
-          title: "beforeViewImpl",
-          kor_name: "화면 미 구현",
-          showInput: false,
-          memos: [
-            {
-              // beforeViewImpl
-              id: "1-1",
-              user_name: "zerochae",
-              content: "test1",
-            },
-          ],
-        },
-        {
-          title: "viewImplemented",
-          kor_name: "화면 구현 완료",
-          showInput: false,
-          memos: [
-            {
-              // viewImplemented
-              id: "2-1",
-              user_name: "zerochae",
-              content: "test2",
-            },
-          ],
-        },
-        {
-          title: "beforeApiImpl",
-          kor_name: "API 개발 전",
-          showInput: false,
-          memos: [
-            {
-              // beforeApiImpl
-              id: "3-1",
-              user_name: "zerochae",
-              content: "test3",
-            },
-          ],
-        },
-        {
-          title: "apiImpling",
-          kor_name: "API 개발 중",
-          showInput: false,
-          memos: [
-            {
-              // apiImpling
-              id: "4-1",
-              user_name: "zerochae",
-              content: "test4",
-            },
-          ],
-        },
-        {
-          title: "apiImplemented",
-          kor_name: "API 개발 완료",
-          showInput: false,
-          memos: [
-            {
-              // apiImplemented
-              id: "5-1",
-              user_name: "zerochae",
-              content: "test5",
-            },
-          ],
-        },
-      ],
+      // listData: [
+      //   {
+      //     title: "beforeViewImpl",
+      //     kor_name: "화면 미 구현",
+      //     showInput: false,
+      //     memos: [
+      //       {
+      //         // beforeViewImpl
+      //         id: "1-1",
+      //         user_name: "zerochae",
+      //         content: "test1",
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     title: "viewImplemented",
+      //     kor_name: "화면 구현 완료",
+      //     showInput: false,
+      //     memos: [
+      //       {
+      //         // viewImplemented
+      //         id: "2-1",
+      //         user_name: "zerochae",
+      //         content: "test2",
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     title: "beforeApiImpl",
+      //     kor_name: "API 개발 전",
+      //     showInput: false,
+      //     memos: [
+      //       {
+      //         // beforeApiImpl
+      //         id: "3-1",
+      //         user_name: "zerochae",
+      //         content: "test3",
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     title: "apiImpling",
+      //     kor_name: "API 개발 중",
+      //     showInput: false,
+      //     memos: [
+      //       {
+      //         // apiImpling
+      //         id: "4-1",
+      //         user_name: "zerochae",
+      //         content: "test4",
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     title: "apiImplemented",
+      //     kor_name: "API 개발 완료",
+      //     showInput: false,
+      //     memos: [
+      //       {
+      //         // apiImplemented
+      //         id: "5-1",
+      //         user_name: "zerochae",
+      //         content: "test5",
+      //       },
+      //     ],
+      //   },
+      // ],
     };
   },
   mounted() {
-    console.log(scene);
+    console.log(kanvan);
+    console.log(this.kanvan);
     console.log(boardTitle_kor);
+
+    this.emitter.on("showCardMenus", (e) => {
+
+      let i = e[0];
+      let j = e[1];
+      let k = this.nowOpen[0];
+      let m = this.nowOpen[1];
+      if(k > -1){
+        this.kanvan.columns[k].cards[m].showCardInMenu = false;
+      }
+      this.kanvan.columns[i].cards[j].showCardInMenu = true;
+      this.nowOpen[0] = i;
+      this.nowOpen[1] = j;
+    });
+  },
+  beforeUpdate() {
   },
   methods: {
     showInput(i) {
       this.input = "";
 
       if (-1 !== this.openInput) {
-        this.scene.children[this.openInput].showInput = false;
+        this.kanvan.columns[this.openInput].showInput = false;
       }
-      this.scene.children[i].showInput = true;
+      this.kanvan.columns[i].showInput = true;
       this.openInput = i;
     },
     getColumnHeightPx() {
-      let kanban = document.getElementById("kanbanContainer");
-      return kanban ? kanban.offsetHeight - 122 : 0;
+      let container = document.getElementById("kanbanContainer");
+      return container ? container.offsetHeight - 122 : 0;
     },
     onColumnDrop(dropResult) {
-      const scene = Object.assign({}, this.scene);
-      scene.children = applyDrag(scene.children, dropResult);
-      this.scene = scene;
+      const kanvan = Object.assign({}, this.kanvan);
+      kanvan.columns = applyDrag(kanvan.columns, dropResult);
+      this.kanvan = kanvan;
     },
     onCardDrop(columnId, dropResult) {
-      // check if element where ADDED or REMOVED in current collumn
+      // check if element where ADDED or REMOVED in current column
       if (dropResult.removedIndex !== null || dropResult.addedIndex !== null) {
-        const scene = Object.assign({}, this.scene);
-        const column = scene.children.filter((p) => p.id === columnId)[0];
-        const itemIndex = scene.children.indexOf(column);
+        const kanvan = Object.assign({}, this.kanvan);
+        const column = kanvan.columns.filter((p) => p.id === columnId)[0];
+        const itemIndex = kanvan.columns.indexOf(column);
         const newColumn = Object.assign({}, column);
 
         // check if element was ADDED in current column
@@ -241,39 +265,38 @@ export default {
           }, Math.random() * 5000 + 1000);
         }
 
-        newColumn.children = applyDrag(newColumn.children, dropResult);
-        scene.children.splice(itemIndex, 1, newColumn);
-        this.scene = scene;
+        newColumn.cards = applyDrag(newColumn.cards, dropResult);
+        kanvan.columns.splice(itemIndex, 1, newColumn);
+        this.kanvan = kanvan;
       }
     },
     getCardPayload(columnId) {
       return (index) => {
-        return this.scene.children.filter((p) => p.id === columnId)[0].children[
+        return this.kanvan.columns.filter((p) => p.id === columnId)[0].cards[
           index
         ];
       };
     },
     addCard(i) {
-      
       console.log(i);
-      var id = this.scene.children.length;
+      var id = this.kanvan.columns.length;
 
-      this.scene.children[i].children.push({
+      this.kanvan.columns[i].cards.push({
         type: "draggable",
         id: `${i}-${id}`,
         user_name: "zerochae",
         content: this.input,
       });
-        this.input = ""
+      this.input = "";
     },
-    removeCard(i){
+    removeCard(i) {
       console.log(i);
-    }
+    },
   },
 };
 </script>
 <style>
-/** NB: dont remove, 
+/** NB: don't remove, 
 * When using orientation="horizontal" it auto sets "display: table"
 * In this case we need flex and not display table  
 */
--- kanvan/src/components/KanbanItem.vue
+++ kanvan/src/components/KanbanItem.vue
@@ -2,23 +2,58 @@
   <Draggable v-if="item">
     <div
       class="
-        cursor-move
-        my-2
-        mx-4
         rounded-lg
         shadow-md
         bg-gray-100
         dark:bg-gray-800
         hover:border-2
         border-primary
+        card-container
       "
     >
-      <div class="p-4 space-y-2">
+      <div
+        v-if="item.loading"
+        class="
+          flex
+          space-x-2
+          bg-gray-50
+          text-gray-900
+          dark:text-gray-200 dark:bg-gray-900
+          px-4
+          py-2
+          rounded-t-lg
+        "
+      >
+        <RefreshIcon
+          class="icons spin"
+          viewBox="0 0 24 24"
+        />
+        <span>Processing...</span>
+      </div>
+      <div class="card-space">
         <div
-          class="rounded-lg bg-primary p-2 w-max h-max inline-block box-content"
+          class="
+            card
+            rounded-lg
+            bg-primary
+            p-2
+            w-max
+            h-max
+            inline-block
+            box-content
+          "
         >
+        <p>
+          {{ emoji }} {{ item.user_name }}
+          <DotsHorizontalIcon @click="showCardMenu(columnIndex,cardIndex)" class="icons" style="float: right" />
+        </p>
         </div>
-        <p>{{ emoji }} {{ item.user_name }}</p>
+        <transition name="showMenu">
+        <div class="card-in-menu" v-if="item.showCardInMenu==true" :id="`cardMenu${columnIndex}${cardIndex}`">
+          <p><PencilIcon class="icons" /> 수정 </p>
+          <p><TrashIcon class="icons" /> 삭제 </p>
+        </div>
+        </transition>
         <p>{{ item.content }}</p>
       </div>
     </div>
@@ -28,20 +63,37 @@
 <script>
 import { getRandomEmoji } from "../utils/helpers";
 import { Draggable } from "vue3-smooth-dnd";
-// import { RefreshIcon } from '@heroicons/vue/outline'
+import {
+  DotsHorizontalIcon,
+  TrashIcon,
+  PencilIcon,
+  RefreshIcon,
+} from "@heroicons/vue/outline";
 export default {
   name: "KanbanItem",
   components: {
     Draggable,
-    //   RefreshIcon
+    DotsHorizontalIcon,
+    TrashIcon,
+    PencilIcon,
+    RefreshIcon,
   },
   data() {
     return {
       emoji: getRandomEmoji(),
+      nowMenu: [-1,-1],
     };
   },
   props: {
     item: Object,
+    columnIndex: Number,
+    cardIndex: Number,
   },
+  methods:{
+    showCardMenu(i,j){
+      var indexArr = [i,j];
+      this.emitter.emit('showCardMenus',indexArr)
+    }
+  }
 };
 </script>
